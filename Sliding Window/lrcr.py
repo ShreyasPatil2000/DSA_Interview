e2f8252a7b8316ccf5e6e14b0568deb99ce517e3
RaydenDarkus <svpatil20000@gmail.com>
2025-05-02 23:14:19 -0400

    next question

--- Sliding Window/lrcr.py
+++ Sliding Window/lrcr.py
@@ -1,11 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        pass
+        res, l = 0, 0
+        maxf = 0
+        count = {}
+        for r, char in enumerate(s):
+            count[char] = count.get(char, 0) + 1
+            maxf = max(maxf, count[char])
+            if (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
     
 ob = Solution()
 print(ob.characterReplacement("ABAB", 2))
 
-#
-
-# Time Complexity:
-# Space Complexity:
+# We initialize variables result, maxf, l and dictionary count. We run an enumerate loop and we calculate frequency of each character. Next we compare max frequency using maxf = max(maxf, count[char]). If the length of substring(r - l + 1) - maxf is greater than number of replacements(k), then we shift left pointer to next character and reduce frequency of character at left pointer. The result is max(result, length of substring). We return the result in the end.
+ 
+# Time Complexity: O(N) As we are running a for loop. The operations inside only take O(1) time complexity
+# Space Complexity: O(1) It will be O(26) as we are updating count for uppercase characters only so round it off to O(1)
